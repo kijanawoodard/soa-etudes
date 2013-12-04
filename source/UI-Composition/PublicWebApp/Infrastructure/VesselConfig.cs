ab97501b5bf90f46c2f452a0f975c4790f74d6c4
Kijana Woodard <kijana.woodard@firehost.com>
2013-12-03 18:23:51 -0600

    with a mediator

--- source/UI-Composition/PublicWebApp/Infrastructure/VesselConfig.cs
+++ source/UI-Composition/PublicWebApp/Infrastructure/VesselConfig.cs
@@ -10,7 +10,10 @@ namespace PublicWebApp.Infrastructure
     {
         public static void RegisterContainer()
         {
+            var mediator = new Mediator();
             var container = new Vessel();
+            container.Register<IMediator>(mediator);
+            container.Register<ISubscribeHandlers>(mediator);
             container.RegisterModules();
 
             DependencyResolver.SetResolver(new VesselDependencyResolver(container)); //for asp.net mvc
