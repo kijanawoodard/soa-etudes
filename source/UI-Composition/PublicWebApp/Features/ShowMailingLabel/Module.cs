fd9f8e94abe21c114d9963ec3b18dcedb69d47d9
Kijana Woodard <github@wyldeye.com>
2013-12-07 00:43:47 -0600

    clean up redundant namespaces
    evaluate whether clarity decreases

--- source/UI-Composition/PublicWebApp/Features/ShowMailingLabel/Module.cs
+++ source/UI-Composition/PublicWebApp/Features/ShowMailingLabel/Module.cs
@@ -3,16 +3,15 @@
 namespace Features.ShowMailingLabel
 {
 	using PublicWebApp.Infrastructure;
-	using Raven.Client;
 
-	public class Module : IModule
+    public class Module : IModule
 	{
 		public void Execute(IContainer container)
 		{
 			container.Register(c => new ShowMailingLabelController(c.Resolve<IMediator>()));
 
 			var mediator = container.Resolve<ISubscribeHandlers>();
-            mediator.Subscribe<Features.ShowMailingLabel.ViewRequest, Features.ShowMailingLabel.ViewModel>(async request => await Execute(request, container.Resolve<Sales.RavenDb>(), container.Resolve<Shipping.RavenDb>()));
+            mediator.Subscribe<ViewRequest, ViewModel>(async request => await Execute(request, container.Resolve<Sales.RavenDb>(), container.Resolve<Shipping.RavenDb>()));
 		}
 
 	    private static async Task<ViewModel> Execute(ViewRequest request, Sales.RavenDb salesDb, Shipping.RavenDb shippingDb)
@@ -25,7 +24,7 @@ namespace Features.ShowMailingLabel
 
                 await Task.WhenAll(sales.Handle(request), shipping.Handle(request));
 
-                var result = new Features.ShowMailingLabel.ViewModel();
+                var result = new ViewModel();
                 result = sales.Handle(result);
                 result = shipping.Handle(result);
                 return result;   
