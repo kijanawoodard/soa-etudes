b94680f885328cc47dce6927639f8785cc7902d8
Kijana Woodard <github@wyldeye.com>
2013-12-06 20:13:13 -0600

    finally, complete dropping the name property
    with the web app updated, we no longer need to support v1 customer

--- source/UI-Composition/Sales.Backend/Program.cs
+++ source/UI-Composition/Sales.Backend/Program.cs
@@ -22,65 +22,25 @@ namespace Sales.Backend
 
 			store.DatabaseCommands.EnsureDatabaseExists(Database);
 
-			//write original data so we can run this version of the project first if desired
-			using (var session = store.OpenSession(Database))
-			{
-				var customer = new V1.Customer
-				{
-					Id = "customers/1234",
-					Name = "John Q. Public"
-				};
-				session.Store(customer);
-				session.SaveChanges();
-			}
-
-			//feature/split-name - It was a mistake to have the name as a single field, split
-			//Take a naive approach. We will let customer care sort out the mess. :-[
+			//feature/fix web app - now that the web app is fixed, drop the Name property
             using (var session = store.OpenSession(Database))
-			using (var enumerator = session.Advanced.Stream<V1.Customer>(fromEtag: Etag.Empty))
+			using (var enumerator = session.Advanced.Stream<V2.Customer>(fromEtag: Etag.Empty))
 			{
 				while (enumerator.MoveNext())
 				{
-					var v1 = enumerator.Current.Document;
-				    var names = v1.Name.Split(' ');
-
-				    var v2 = new V2.Customer();
-				    v2.Id = v1.Id;
-				    v2.FirstName = names[0];
-
-				    if (names.Length == 2)
-				    {
-				        v2.LastName = names[1];
-				    }
-                    else if (names.Length > 2)
-                    {
-                        v2.MiddleName = names[1];
-                        v2.LastName = names[2];
-                    }
-
+					var v2 = enumerator.Current.Document;
                     session.Store(v2);
-                    session.SaveChanges();
+				    session.SaveChanges();
 				}
 			}
-
 		}
 	}
-
-    namespace V1
-    {
-        public class Customer
-        {
-            public string Id { get; set; }
-            public string Name { get; set; }
-        }
-    }
 
     namespace V2
     {
         public class Customer
         {
             public string Id { get; set; }
-            public string Name { get { return string.Format("{0} {1} {2}", FirstName, MiddleName, LastName); } }
             public string FirstName { get; set; }
             public string MiddleName { get; set; }
             public string LastName { get; set; }
