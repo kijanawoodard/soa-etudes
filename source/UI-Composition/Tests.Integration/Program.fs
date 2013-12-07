592b80c9ac5c1fea13a58beb23e07f154950391c
Kijana Woodard <github@wyldeye.com>
2013-12-06 18:25:03 -0600

    write a test that exposes the problem
    from the user's point of view

---
+++ source/UI-Composition/Tests.Integration/Program.fs
@@ -0,0 +1,16 @@
+﻿
+open canopy
+open runner
+open configuration
+
+start firefox
+compareTimeout <- 1.0
+
+"Look for name" &&& fun _ ->
+    url "http://localhost:18572/"
+    ".name" != ""
+
+run()
+
+quit()
+
